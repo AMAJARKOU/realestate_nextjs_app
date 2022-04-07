07c7405dcb4f4acda37231f476ea1df062ad9802
Essaid AMAJARKOU <amajarkou@mobiblanc.com>
2022-04-07 12:18:09 +0000

    Search filtering last update

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 import { Flex, Box, Select, Text, Input, Spinner, Icon, Button } from "@chakra-ui/react";
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/router";
 import {MdCancel} from 'react-icons/md';
 import Image from "next/image";
 import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
     const [filters, setFilters] = useState(filterData);
-    const searchProperties = () => {
+    const router = useRouter();
+    const searchProperties = (filterValues) => {
+        const path = router.pathname;
+        const { query } = router;
+        const values = getFilterValues(filterValues);
 
+        values.forEach((item) => {
+            query[item.name] = item.value;
+        });
+
+        router.push({pathname: path, query});
     }
     return (
         <Flex bg={"gray.100"} p={"4"} justifyContent={"center"} flexWrap={"wrap"}>
